218a36ecfcba3b13fa993e103f90a549f302ccae
Manuel Turetta <manuel.turetta94@gmail.com>
2022-12-09 16:58:23 -0500

    refactor: removed constants

--- examples/wallet-example/src/consts.ts
+++
@@ -1,2 +0,0 @@
-export const DEFAULT_MNEMONIC = "hour harbor fame unaware bunker junk garment decrease federal vicious island smile warrior fame right suit portion skate analyst multiply magnet medal fresh sweet";
-
