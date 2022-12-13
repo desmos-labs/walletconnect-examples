36f01246d8ef0fd14dde4dc86711635ac1f55669
Manuel Turetta <manuel.turetta94@gmail.com>
2022-12-13 15:15:43 -0500

    feat: web3auth example

---
+++ examples/react-web3auth/src/components/Header.tsx
@@ -0,0 +1,49 @@
+import React, {useCallback, useEffect, useState} from "react";
+import {AppBar, Button, Toolbar, Typography,} from "@mui/material";
+import {useSignerContext} from "../context/signercontext";
+import useSignerStatus from "../hooks/useSignerStatus";
+import {SignerStatus} from "@desmoslabs/desmjs";
+
+export default function Header(): JSX.Element {
+  const {connect, disconnect, signer} = useSignerContext();
+  const signerStatus = useSignerStatus();
+  const [address, setAddress] = useState("");
+
+  const onClick = useCallback(() => {
+    if (signerStatus === SignerStatus.Connected) {
+      disconnect();
+    } else if (signerStatus === SignerStatus.NotConnected) {
+      connect();
+    }
+  }, [connect, disconnect, signerStatus]);
+
+  useEffect(() => {
+    if (signer !== undefined && signerStatus === SignerStatus.Connected) {
+      signer.getAccounts().then(accounts => {
+        setAddress(accounts[0].address);
+      })
+    } else {
+      setAddress("");
+    }
+  }, [signerStatus])
+
+  const connectDisabled = signerStatus !== SignerStatus.Connected && signerStatus !== SignerStatus.NotConnected;
+
+  return <AppBar position="static">
+    <Toolbar>
+      <Typography variant="h6" sx={{mr: 2}}>
+        Profile Manager
+      </Typography>
+      <Typography
+        variant="caption"
+        component="div"
+        sx={{flexGrow: 1, textAlign: "end"}}
+      >
+        {address}
+      </Typography>
+      <Button color="inherit" onClick={onClick} disabled={connectDisabled}>
+        {signerStatus === SignerStatus.Connected ? "Disconnect" : "Connect"}
+      </Button>
+    </Toolbar>
+  </AppBar>
+}
